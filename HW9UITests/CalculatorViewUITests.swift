2f4879a69287ba4f9e85a80a5a4d7ee5fa37e962
AndrRok <rokhmanov2000@yandex.ru>
2024-08-07 16:19:54 +0400

    HW9

--- HW9UITests/CalculatorViewUITests.swift
+++ HW9UITests/CalculatorViewUITests.swift
@@ -1,73 +1,73 @@
-////
-////  CalculatorViewUITests.swift
-////  HW9UITests
-////
-////  Created by ARMBP on 8/7/24.
-////
 //
-//import XCTest
+//  CalculatorViewUITests.swift
+//  HW9UITests
 //
-//class CalculatorViewUITests: XCTestCase {
+//  Created by ARMBP on 8/7/24.
 //
-//    var app: XCUIApplication!
-//
-//    override func setUpWithError() throws {
-//        continueAfterFailure = false
-//        app = XCUIApplication()
-//        app.launch()
-//    }
-//
-//    func testAddition() throws {
-//        let display = app.staticTexts["display"]
-//        
-//        app.buttons["1"].tap()
-//        app.buttons["+"].tap()
-//        app.buttons["2"].tap()
-//        app.buttons["="].tap()
-//        
-//        XCTAssertEqual(display.label, "3.0")
-//    }
-//
-//    func testSubtraction() throws {
-//        let display = app.staticTexts["display"]
-//        
-//        app.buttons["5"].tap()
-//        app.buttons["-"].tap()
-//        app.buttons["3"].tap()
-//        app.buttons["="].tap()
-//        
-//        XCTAssertEqual(display.label, "2.0")
-//    }
-//
-//    func testMultiplication() throws {
-//        let display = app.staticTexts["display"]
-//        
-//        app.buttons["4"].tap()
-//        app.buttons["*"].tap()
-//        app.buttons["2"].tap()
-//        app.buttons["="].tap()
-//        
-//        XCTAssertEqual(display.label, "8.0")
-//    }
-//
-//    func testDivision() throws {
-//        let display = app.staticTexts["display"]
-//        
-//        app.buttons["8"].tap()
-//        app.buttons["/"].tap()
-//        app.buttons["2"].tap()
-//        app.buttons["="].tap()
-//        
-//        XCTAssertEqual(display.label, "4.0")
-//    }
-//
-//    func testClear() throws {
-//        let display = app.staticTexts["display"]
-//        
-//        app.buttons["1"].tap()
-//        app.buttons["C"].tap()
-//        
-//        XCTAssertEqual(display.label, "0")
-//    }
-//}
-//
+
+import XCTest
+
+class CalculatorViewUITests: XCTestCase {
+
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+
+    func testAddition() throws {
+        let display = app.staticTexts["display"]
+        
+        app.buttons["1"].tap()
+        app.buttons["+"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(display.label, "3.0")
+    }
+
+    func testSubtraction() throws {
+        let display = app.staticTexts["display"]
+        
+        app.buttons["5"].tap()
+        app.buttons["-"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(display.label, "2.0")
+    }
+
+    func testMultiplication() throws {
+        let display = app.staticTexts["display"]
+        
+        app.buttons["4"].tap()
+        app.buttons["*"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(display.label, "8.0")
+    }
+
+    func testDivision() throws {
+        let display = app.staticTexts["display"]
+        
+        app.buttons["8"].tap()
+        app.buttons["/"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(display.label, "4.0")
+    }
+
+    func testClear() throws {
+        let display = app.staticTexts["display"]
+        
+        app.buttons["1"].tap()
+        app.buttons["C"].tap()
+        
+        XCTAssertEqual(display.label, "0")
+    }
+}
+
